a462fb4f998144972826c12098e5227d902f8411
adfcf <adfcf@aluno.ifnmg.edu.br>
2022-05-11 19:15:30 -0300

    Adicionado operacoes.java

---
+++ variaveis-tipificacao/operacoes.java
@@ -0,0 +1,54 @@
+public class Main {
+	public static void main(String[] args) {
+		
+	/*		 
+	 * Operações aritméticas
+	 */
+		
+        int inteiro1 = 10;
+        int inteiro2 = 25;
+        
+        // +
+        int soma = inteiro1 + inteiro2;
+        System.out.println(soma);
+        
+        // -
+        int subtracao = inteiro1 - inteiro2;
+        System.out.println(subtracao);
+        
+        // *
+        int multiplicacao = inteiro1 * inteiro2;
+        System.out.println(multiplicacao);
+        
+        // /
+        int divisao = inteiro1 / inteiro2;
+        System.out.println(divisao);
+        
+        // %
+        int resto = inteiro2 % inteiro1;
+        System.out.println(resto);
+        
+        // ++ e --
+        int posIncremento = inteiro1++;
+        int preIncremento = ++inteiro1;
+        int posDecremento = inteiro1--;
+        int preDecremento = --inteiro1;
+        
+        System.out.println(posIncremento);
+        System.out.println(preDecremento);
+        
+        /*
+	 * Operações de atribuição aritméticas
+ 	 */
+		
+		double d1 = 1.0d;
+		double d2 = 2.0d;
+		d1 += 15.5;
+		d1 -= d2;
+		d1 *= 2.5d;
+		d1 /= 0.67d;
+		
+		System.out.println(d2);
+
+	}
+}
