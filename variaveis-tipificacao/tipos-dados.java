3de4ff3cb33bf5a9e73599b71097038c46db42dd
adfcf <adfcf@aluno.ifnmg.edu.br>
2022-05-11 18:47:30 -0300

    Adicionado arquivo de tipos de dados

---
+++ variaveis-tipificacao/tipos-dados.java
@@ -0,0 +1,39 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		/*
+		 * Tipos primitivos de dados em Java
+		*/
+		
+		// inteiro de 1 byte
+		byte b0 = 10;
+		// inteiro de 2 bytes
+		short s0 = -56;
+		// inteiro de 4 bytes
+		int i0 = 12352;
+		// inteiro de 8 bytes
+		long l0 = -1232431;
+		
+		// valor lógico (1 byte)
+		boolean b1 = true;
+		
+		// caractere (2 bytes)
+		char c0 = 'a';
+		
+		// número fracionário de precisão simples
+		final float PI = 3.141592f;
+		// número fracionário de precisão dupla
+		double d0 = 3.33;
+		
+		// imprimindo os valores
+		System.out.println("byte b0 = " + b0);
+		System.out.println("short s0 = " + s0);
+		System.out.println("int i0 = " + i0);
+		System.out.println("long l0 = " + l0);
+		System.out.println("char c0 = " + c0);
+		System.out.println("boolean b0 = " + b1);
+		System.out.println("float PI = " + PI);
+		System.out.println("double d0 = " + d0);
+
+	}
+}
