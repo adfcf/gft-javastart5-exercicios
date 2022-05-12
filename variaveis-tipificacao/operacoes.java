dd952e3c42605e6e6fe8bbb590f3a2abbaacd273
adfcf <adfcf@aluno.ifnmg.edu.br>
2022-05-12 17:44:11 -0300

    Atualizado operacoes.java

--- variaveis-tipificacao/operacoes.java
+++ variaveis-tipificacao/operacoes.java
@@ -59,6 +59,51 @@ public static void main(String[] args) {
 	System.out.println(precedencia2);
 	System.out.println(precedencia3);
 
+	/*
+	* OPERADOES RELACIONAIS
+	*/
+		
+	int a = 1;
+	int b = 2;
+		
+	// igualdade
+	boolean f1 = a == b;
+		
+	// desigualdade
+	boolean f2 = a != b;
+				
+	// maior que - maior ou igual que
+	boolean f3 = a > b;
+	boolean f4 = a >= b;
+		
+	// menor que - menor ou igual que
+	boolean f5 = a < b;
+	boolean f6 = a <= b;
+		
+	System.out.println(f1);
+	System.out.println(f2);
+	System.out.println(f3);
+	System.out.println(f4);
+	System.out.println(f5);
+	System.out.println(f6);
+
+	/*
+	* OPERADORES LÓGICOS
+	*/
+
+	// conjunção lógica
+	boolean and = f1 && f2;
+
+	// disjunção inclusiva lógica
+	boolean or = f1 || f2;
+
+	// negação lógica
+	boolean not = !f1;
+
+	System.out.println(and);
+	System.out.println(or);
+	System.out.println(not);
+
 }
 
 }
