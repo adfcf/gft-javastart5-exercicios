cda4c80188a931ec54eac7d60b8a53714ffa0e29
adfcf <adfcf@aluno.ifnmg.edu.br>
2022-05-13 06:41:57 -0300

    Adicionado estruturas iterativas.java

---
+++ controle-fluxo/estruturas-iterativas.java
@@ -0,0 +1,27 @@
+public class Main
+{
+public static void main(String[] args) {
+
+        /*
+        * ESTRUTURAS ITERATIVAS DE CONTROLE DE FLUXO 
+        */
+        
+        boolean condicao = true;
+        
+        // Estrutura iterativa pré-condicionada (while)
+        while (condicao) {
+            // ...
+        }
+		
+		// Estrutura iterativa pós-condicionada (do-while)
+        do {
+            // ...
+        } while (condicao);
+		
+		// Estrutura iterativa enumerada (for)
+        for (int i = 0; i < 10; ++i) {
+            // ...
+        }
+		
+}
+}
